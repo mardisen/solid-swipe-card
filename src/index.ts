19b831fd01b1a9adda675dff7794b206444814a4
mardisen <marino.didio.work@gmail.com>
2022-07-21 19:03:28 +0200

    Added TODO

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import createSwipeCard from './createSwipeCard';
 import { SwipeCard } from './SwipeCard';
 import { SwipeCardProps, SwipeCardRef, SwipeDirection } from './types';
 
+// TODO: Add SwipeCardList
 export { SwipeCard, createSwipeCard, SwipeCardRef, SwipeCardProps, SwipeDirection };
