6c870e95521b59fe65bf80217acf77696f4303e3
mardisen <marino.didio.work@gmail.com>
2022-08-08 08:12:32 +0200

    Added test

--- test/SwipeCard.test.tsx
+++ test/SwipeCard.test.tsx
@@ -1,11 +1,30 @@
-import { render } from 'solid-testing-library';
+import { render, fireEvent } from 'solid-testing-library';
 
 import { SwipeCard } from '../src';
 
 describe('SwipeCard', () => {
     it('renders a div', () => {
-        const { getByTestId, unmount } = render(() => <SwipeCard id='test-id' />);
+        const { getByTestId, unmount } = render(() => <SwipeCard id="test-id" />);
         expect(getByTestId('test-id')).toBeInTheDocument();
+        unmount();
+    });
+
+    it('can be grabbed', () => {
+        const { getByTestId, unmount } = render(() => <SwipeCard id="test-id" />);
+        const element = getByTestId('test-id') as HTMLElement;
+
+        const initialPosition = {
+            x: element.getBoundingClientRect().left,
+            y: element.getBoundingClientRect().top
+        };
+        fireEvent.mouseDown(element);
+        fireEvent.mouseUp(element);
+        const finalPosition = {
+            x: element.getBoundingClientRect().left,
+            y: element.getBoundingClientRect().top
+        };
+
+        expect(initialPosition).toEqual(finalPosition);
         unmount();
     });
 });
